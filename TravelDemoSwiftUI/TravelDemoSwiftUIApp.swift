48edb868dd2a3a00e3ced3eab8bb0fee84bdfd1c
Youssef Ahmed <youssefahmed459@gmail.com>
2022-11-10 20:52:53 +0200

    Initial Commit

---
+++ TravelDemoSwiftUI/TravelDemoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelDemoSwiftUIApp.swift
+//  TravelDemoSwiftUI
+//
+//  Created by youssef on 10/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct TravelDemoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

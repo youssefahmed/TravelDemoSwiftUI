3e19f0a5b27c483980c9b44d91d760b003c8f453
Youssef Ahmed <youssefahmed459@gmail.com>
2022-11-10 21:42:56 +0200

    add discover + map kit

---
+++ TravelDemoSwiftUI/Modules/Discover/PopularDestinationDetailsView.swift
@@ -0,0 +1,168 @@
+//
+//  File.swift
+//  TravelDemoSwiftUI
+//
+//  Created by youssef on 10/11/2022.
+//
+
+import SwiftUI
+import MapKit
+
+struct PopularDestinationDetailsView: View {
+    
+    let destination: Destination
+    @ObservedObject var vm: DestinationDetailsViewModel
+    @State var region: MKCoordinateRegion
+    @State var isShowingAttractions = true
+    
+    init(destination: Destination) {
+        print("Hitting network unnecessarily")
+        self.destination = destination
+        self._region = State(initialValue: MKCoordinateRegion(center: .init(latitude: destination.latitude, longitude: destination.longitude), span: .init(latitudeDelta: 0.1, longitudeDelta: 0.1)))
+        
+        self.vm = .init(name: destination.name)
+    }
+    
+    let imageUrlStrings = [
+        "https://letsbuildthatapp-videos.s3-us-west-2.amazonaws.com/2240d474-2237-4cd3-9919-562cd1bb439e",
+        "https://letsbuildthatapp-videos.s3-us-west-2.amazonaws.com/b1642068-5624-41cf-83f1-3f6dff8c1702",
+        "https://letsbuildthatapp-videos.s3-us-west-2.amazonaws.com/6982cc9d-3104-4a54-98d7-45ee5d117531"
+    ]
+    
+    var body: some View {
+        ScrollView {
+            
+            if let photos = vm.destinationDetails?.photos {
+                DestinationHeaderContainer(imageUrlStrings: photos)
+                    .frame(height: 250)
+            }
+            VStack(alignment: .leading) {
+                Text(destination.name)
+                    .font(.system(size: 18, weight: .bold))
+                Text(destination.country)
+                
+                HStack {
+                    ForEach(0..<5, id: \.self) { num in
+                        Image(systemName: "star.fill")
+                            .foregroundColor(.orange)
+                    }
+                }.padding(.top, 2)
+                HStack {
+                    Text(vm.destinationDetails?.description ?? "")
+                        .padding(.top, 4)
+                        .font(.system(size: 14))
+                    Spacer()
+                    
+                }
+            }
+            .padding(.horizontal)
+            
+            HStack {
+                Text("Location")
+                    .font(.system(size: 18, weight: .semibold))
+                Spacer()
+                
+                Button(action: { isShowingAttractions.toggle() }, label: {
+                    Text("\(isShowingAttractions ? "Hide" : "Show") Attractions")
+                        .font(.system(size: 12, weight: .semibold))
+                })
+                
+               
+                Toggle("", isOn: $isShowingAttractions)
+                    .labelsHidden()
+                
+            }.padding(.horizontal)
+            
+            Map(coordinateRegion: $region, annotationItems: isShowingAttractions ? attractions : []) { attraction in
+                MapAnnotation(coordinate: .init(latitude: attraction.latitude, longitude: attraction.longitude)) {
+                    CustomMapAnnotation(attraction: attraction)
+                }
+            }
+            .frame(height: 300)
+            
+        }.navigationBarTitle(destination.name, displayMode: .inline)
+    }
+    
+    let attractions: [Attraction] = [
+        .init(name: "Eiffel Tower", imageName: "eiffel_tower", latitude: 48.858605, longitude: 2.2946),
+        .init(name: "Champs-Elysees", imageName: "new_york", latitude: 48.866867, longitude: 2.311780),
+        .init(name: "Louvre Museum", imageName: "art2", latitude: 48.860288, longitude: 2.337789)
+    ]
+}
+
+struct CustomMapAnnotation: View {
+    
+    let attraction: Attraction
+    
+    var body: some View {
+        VStack {
+            Image(attraction.imageName)
+                .resizable()
+                .frame(width: 80, height: 60)
+                .cornerRadius(4)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 4)
+                        .stroke(Color(.init(white: 0, alpha: 0.5)))
+                )
+            Text(attraction.name)
+                .font(.system(size: 12, weight: .semibold))
+                .padding(.horizontal, 6)
+                .padding(.vertical, 4)
+                .background(LinearGradient(gradient: Gradient(colors: [Color.gray, Color.gray]), startPoint: .leading, endPoint: .trailing))
+                .foregroundColor(.white)
+//                .border(Color.black)
+                .cornerRadius(4)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 4)
+                        .stroke(Color(.init(white: 0, alpha: 0.5)))
+                )
+            
+        }.shadow(radius: 5)
+    }
+}
+
+struct Attraction: Identifiable {
+    let id = UUID().uuidString
+    let name, imageName: String
+    let latitude, longitude: Double
+}
+
+struct PopularDestinationTile: View {
+    
+    let destination: Destination
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 0) {
+            
+            Image(destination.imageName)
+                .resizable()
+                .scaledToFill()
+                .frame(width: 125, height: 125)
+                .cornerRadius(4)
+                .padding(.horizontal, 6)
+                .padding(.vertical, 6)
+            
+            Text(destination.name)
+                .font(.system(size: 12, weight: .semibold))
+                .padding(.horizontal, 12)
+                .foregroundColor(Color(.label))
+            
+            Text(destination.country)
+                .font(.system(size: 12, weight: .semibold))
+                .padding(.horizontal, 12)
+                .padding(.bottom, 8)
+                .foregroundColor(.gray)
+        }
+        .asTile()
+    }
+}
+
+struct PopularDestinationsView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            PopularDestinationDetailsView(destination: .init(name: "Paris", country: "France", imageName: "eiffel_tower", latitude: 48.859565, longitude: 2.353235))
+        }
+        DiscoverView()
+        PopularDestinationsView()
+    }
+}

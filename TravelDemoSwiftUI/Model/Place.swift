bff2ca8d36dfe2e405efe521219ce79701197618
Youssef Ahmed <youssefahmed459@gmail.com>
2022-11-10 21:04:59 +0200

    add models dto

---
+++ TravelDemoSwiftUI/Model/Place.swift
@@ -0,0 +1,12 @@
+//
+//  Place.swift
+//  TravelDemoSwiftUI
+//
+//  Created by youssef on 10/11/2022.
+//
+
+import Foundation
+
+struct Place: Decodable, Hashable {
+    let name, thumbnail: String
+}
